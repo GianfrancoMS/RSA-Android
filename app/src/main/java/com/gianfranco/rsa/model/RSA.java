85cc1234084d0e899b2de7cc50bd4e124665266a
GianfrancoMS <gianfrancomonsal@gmail.com>
2018-03-09 10:55:14 -0500

    Add RSA Builder

--- app/src/main/java/com/gianfranco/rsa/model/RSA.java
+++ app/src/main/java/com/gianfranco/rsa/model/RSA.java
@@ -13,54 +13,17 @@ import java.security.interfaces.RSAPublicKey;
 
 import javax.crypto.Cipher;
 
-public class RSA implements Serializable {
+import static com.gianfranco.rsa.model.Utils.checkNotNull;
 
-    private final BigInteger P;
-    private final BigInteger Q;
-    private final BigInteger N;
-    private final BigInteger Z;
-    private final BigInteger D;
-    private final BigInteger E;
-    private final String encryptedText;
-    private final String decryptedText;
-
-    public RSA(int keySize, String plaintText) throws Exception {
-        if (!isValidText(plaintText))
-            throw new Exception("Invalid string for encryption");
-
-        if (!isValidBits(keySize))
-            throw new Exception("Invalid bits for encryption");
-
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-        kpg.initialize(keySize);
-        KeyPair kp = kpg.generateKeyPair();
-        PublicKey publicKey = kp.getPublic();
-        PrivateKey privateKey = kp.getPrivate();
-
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encodedBytes = cipher.doFinal(plaintText.getBytes());
-        encryptedText = Base64.encodeToString(encodedBytes, Base64.DEFAULT);
-
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] decodedBytes = cipher.doFinal(encodedBytes);
-        decryptedText = new String(decodedBytes);
-
-        P = ((RSAPrivateCrtKey) privateKey).getPrimeP();
-        Q = ((RSAPrivateCrtKey) privateKey).getPrimeQ();
-        N = ((RSAPrivateCrtKey) privateKey).getModulus();
-        Z = (P.subtract(BigInteger.ONE)).multiply(Q.subtract(BigInteger.ONE));
-        E = ((RSAPublicKey) publicKey).getPublicExponent();
-        D = E.modInverse(Z);
-    }
-
-    private boolean isValidText(String text) {
-        return text != null && !text.isEmpty() && !text.trim().isEmpty();
-    }
-
-    private boolean isValidBits(int bits) {
-        return bits > 0 && (bits & (bits - 1)) == 0;
-    }
+public final class RSA implements Serializable {
+    BigInteger P;
+    BigInteger Q;
+    BigInteger N;
+    BigInteger Z;
+    BigInteger D;
+    BigInteger E;
+    String encryptedText;
+    String decryptedText;
 
     public String pToString() {
         return P.toString();
@@ -94,7 +57,102 @@ public class RSA implements Serializable {
         return decryptedText;
     }
 
-    public static class RSABuilder{
+    public static final class Builder {
+        private final String RSA = "RSA";
 
+        private final String message;
+
+        private byte[] encodedBytes;
+        private byte[] decodedBytes;
+
+        private String encryptedText;
+        private String decryptedText;
+
+        private final KeyPairGenerator keyPairGenerator;
+        private final KeyPair keyPair;
+        private final PublicKey publicKey;
+        private final PrivateKey privateKey;
+
+        public Builder(int keySize, String message) {
+            try {
+                if (!areValidBits(keySize))
+                    throw new IllegalArgumentException("Invalid bits for encryption");
+
+                if (!isValidMessage(message))
+                    throw new IllegalArgumentException("Invalid message for encryption");
+
+                this.message = message;
+
+                keyPairGenerator = KeyPairGenerator.getInstance(RSA);
+                keyPairGenerator.initialize(keySize);
+                keyPair = keyPairGenerator.generateKeyPair();
+                publicKey = keyPair.getPublic();
+                privateKey = keyPair.getPrivate();
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+        public Builder encodeMessage() {
+            try {
+                Cipher cipher = Cipher.getInstance(RSA);
+                cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+                encodedBytes = cipher.doFinal(message.getBytes());
+                encryptedText = Base64.encodeToString(encodedBytes, Base64.DEFAULT);
+
+                return this;
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+        public Builder decodeMessage() {
+            try {
+                checkNotNull(encodedBytes, "encodedBytes == null. You must call encodeMessage() before");
+
+                Cipher cipher = Cipher.getInstance(RSA);
+                cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+                decodedBytes = cipher.doFinal(encodedBytes);
+                decryptedText = new String(decodedBytes);
+
+                return this;
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+        public RSA build() {
+            checkNotNull(encodedBytes, "encodedBytes == null. You must call encodeMessage() before");
+            checkNotNull(decodedBytes, "decodedBytes == null. You must call decodeMessage() before");
+
+            BigInteger P = ((RSAPrivateCrtKey) privateKey).getPrimeP();
+            BigInteger Q = ((RSAPrivateCrtKey) privateKey).getPrimeQ();
+            BigInteger N = ((RSAPrivateCrtKey) privateKey).getModulus();
+            BigInteger Z = (P.subtract(BigInteger.ONE)).multiply(Q.subtract(BigInteger.ONE));
+            BigInteger E = ((RSAPublicKey) publicKey).getPublicExponent();
+            BigInteger D = E.modInverse(Z);
+
+            RSA rsa = new RSA();
+            rsa.P = P;
+            rsa.Q = Q;
+            rsa.N = N;
+            rsa.Z = Z;
+            rsa.E = E;
+            rsa.D = D;
+            rsa.encryptedText = encryptedText;
+            rsa.decryptedText = decryptedText;
+
+            return rsa;
+        }
+
+        private boolean isValidMessage(String message) {
+            return message != null && !message.isEmpty() && !message.trim().isEmpty();
+        }
+
+        private boolean areValidBits(int bits) {
+            return bits > 0 && (bits & (bits - 1)) == 0;
+        }
     }
 }
